501e1d7f7fe46fbad42f285574eecfe9bf4b2656
AtasagunSanap <63560730+AtasagunSanap@users.noreply.github.com>
2020-04-28 01:58:21 +0300

    Update HomeOptionsPanel.java

--- HomeOptionsPanel.java
+++ HomeOptionsPanel.java
@@ -1,11 +1,24 @@
+import java.awt.GraphicsEnvironment;
+
 import javax.swing.*;
 import javax.swing.border.LineBorder;
+
 import java.awt.*;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class HomeOptionsPanel extends JPanel
 {
-    public HomeOptionsPanel()
-    {
+    EditorAreaPanel display;
+    JComboBox fontComboBox;
+    Font displayFont;
+    String fontList[] = GraphicsEnvironment.getLocalGraphicsEnvironment().getAvailableFontFamilyNames();
+
+    public HomeOptionsPanel( EditorAreaPanel display)
+    {        
+        this.display = display;
+        
         setPreferredSize( new Dimension( 200, 130 ) );
         setBorder( BorderFactory.createLineBorder( Color.BLUE) );
         setBackground( Color.darkGray );
@@ -24,8 +37,13 @@ public class HomeOptionsPanel extends JPanel
         fontComboBox.setPreferredSize( new Dimension(94, 20) );
         fontComboBox.setBackground(Color.CYAN);
         fontComboBox.setToolTipText("");
-        fontComboBox.addItem(" Font ");
+        for (int i = 0; i < fontList.length; i++) {
+            fontComboBox.addItem(fontList[i]);
+            if( display.getEditorFont().getFontName().equals(fontList[i]))
+                fontComboBox.setSelectedItem( fontList[i]);
+        }
         add ( fontComboBox );
+        fontComboBox.addActionListener(new FontListener());
 
         JComboBox sizeComboBox = new JComboBox();
         sizeComboBox.setPreferredSize( new Dimension(94, 20) );
@@ -34,15 +52,50 @@ public class HomeOptionsPanel extends JPanel
         sizeComboBox.addItem(" Size ");
         add( sizeComboBox );
 
-        JButton commentingAndUncommenting = new JButton("Comment/Uncomment Lines");
+        JButton commentingAndUncommenting = new JButton("Comment");
         commentingAndUncommenting.setBackground(Color.CYAN);
         commentingAndUncommenting.setBounds(0, 112, 101, 40);
         add ( commentingAndUncommenting );
+        commentingAndUncommenting.addActionListener( new CommentListener());
 
         JButton indentAll = new JButton("Indent All");
         indentAll.setBackground(Color.CYAN);
         indentAll.setPreferredSize( new Dimension( 195, 25) );
         add ( indentAll );
         
+    }
+        
+    class FontListener implements ActionListener {
+        String fontName;
+        public void actionPerformed( ActionEvent e) {
+            fontName = (String) ((JComboBox) e.getSource()).getSelectedItem();
+            displayFont = display.getEditorFont();
+            display.setEditorFont( new Font( fontName, displayFont.getStyle(), displayFont.getSize() ));
+        }
+    }
+
+    class CommentListener implements ActionListener {
+        int begin;
+        int end;
+        String content;
+        boolean isCommented;
+
+        public void actionPerformed( ActionEvent e) {
+            begin = display.getSelectionFirst();
+            end = display.getSelectionLast();
+            content = display.getContent();
+            if( begin != end) {
+                for (int newLine = begin; newLine <= end; newLine++) {
+                    if (!content.substring(newLine, newLine + 2).equals("//")) {
+                        if (content.substring(newLine - 2, newLine - 1).equals("\n")) {
+                            content = content.substring(0, newLine ) + "//" + content.substring(newLine);
+                        }
+                    } else {
+                        content = content.substring(0, newLine) + content.substring(newLine + 2);
+                    }
+                }
+            }
+            display.setContent( content);
+        }
     }
 }
