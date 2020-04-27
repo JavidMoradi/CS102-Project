d201b79a65629bf3ffac9dfbc788e40052b665a3
AtasagunSanap <63560730+AtasagunSanap@users.noreply.github.com>
2020-04-28 01:51:05 +0300

    Update CodeReviewerFrame.java

--- CodeReviewerFrame.java
+++ CodeReviewerFrame.java
@@ -3,14 +3,16 @@ import java.awt.*;
 
 public class CodeReviewerFrame extends JFrame
 {
-    FileOptionsPanel fileOptionsPanel = new FileOptionsPanel();
+    EditorAreaPanel display = new EditorAreaPanel();
+    FileOptionsPanel fileOptionsPanel = new FileOptionsPanel( display);
     public CodeReviewerFrame ( String title )
     {
         super(title);
         setDefaultCloseOperation ( JFrame.EXIT_ON_CLOSE );
         setPreferredSize( new Dimension( 1500, 1000 ) );
         
-        add ( new HomeOptionsPanel () );
+        add(display);
+        add ( new HomeOptionsPanel ( display));
         
         add ( new NewCommentPanel () );
         add ( new CommentOptionsPanel(), BorderLayout.EAST);
