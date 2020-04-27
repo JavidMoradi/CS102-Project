002b8537d4a15f68285522926f5d18e8683fc985
oavci2000 <63664598+oavci2000@users.noreply.github.com>
2020-04-28 02:42:32 +0300

    Update NewCommentPanel.java

--- NewCommentPanel.java
+++ NewCommentPanel.java
@@ -1,6 +1,7 @@
 import javax.swing.*;
 import javax.swing.border.LineBorder;
 import java.awt.*;
+import java.awt.event.*;
 
 public class NewCommentPanel extends JPanel
 {
@@ -25,36 +26,78 @@ public class NewCommentPanel extends JPanel
         wrongIndentationError.setBackground(Color.RED);
         wrongIndentationError.setForeground( Color.WHITE );
         wrongIndentationError.setPreferredSize( new Dimension( 190, 25) );
+        wrongIndentationError.addActionListener(new ActionListener() {
+        	public void actionPerformed(ActionEvent e) {
+                EditorAreaPanel.addHighlight( Color.RED);
+                CommentShowPanel.addComment( "Line ... : Wrong Identation Error." );
+        	}
+
+        });
         add ( wrongIndentationError );
 
         JButton inefficientError = new JButton("Inefficient Code");
         inefficientError.setBackground(Color.BLUE);
         inefficientError.setForeground( Color.WHITE );
         inefficientError.setPreferredSize( new Dimension( 170, 25) );
+        inefficientError.addActionListener(new ActionListener() {
+        	public void actionPerformed(ActionEvent e) {
+                EditorAreaPanel.addHighlight( Color.BLUE);
+                CommentShowPanel.addComment( "Line ... : Inefficient Code Error." );
+        	}
+
+        });
         add ( inefficientError );
 
         JButton namingConventionError = new JButton("Naming Conventions Error");
         namingConventionError.setBackground(Color.MAGENTA);
         namingConventionError.setForeground( Color.WHITE );
         namingConventionError.setPreferredSize( new Dimension( 190, 25) );
+        namingConventionError.addActionListener(new ActionListener() {
+        	public void actionPerformed(ActionEvent e) {
+                EditorAreaPanel.addHighlight( Color.MAGENTA);
+                CommentShowPanel.addComment( "Line ... : Nameing Convention Error." );
+        	}
+
+        });
         add ( namingConventionError );
 
         JButton javaDocError = new JButton("JavaDoc Error");
         javaDocError.setBackground(Color.yellow);
         javaDocError.setForeground( Color.BLACK );
         javaDocError.setPreferredSize( new Dimension( 170, 25) );
+        javaDocError.addActionListener(new ActionListener() {
+        	public void actionPerformed(ActionEvent e) {
+                EditorAreaPanel.addHighlight( Color.yellow);
+                CommentShowPanel.addComment( "Line ... : JavaDoc Comment Error." );
+        	}
+
+        });
         add ( javaDocError );
 
         JButton styleErrorButton = new JButton("Blank Line/Space Error");
         styleErrorButton.setBackground(Color.gray);
         styleErrorButton.setForeground( Color.WHITE );
         styleErrorButton.setPreferredSize( new Dimension( 190, 25) );
+        styleErrorButton.addActionListener(new ActionListener() {
+        	public void actionPerformed(ActionEvent e) {
+                EditorAreaPanel.addHighlight( Color.gray);
+                CommentShowPanel.addComment( "Line ... : Style Error." );
+        	}
+
+        });
         add ( styleErrorButton );
 
         JButton commentError = new JButton("Comment Error");
         commentError.setBackground(Color.black);
         commentError.setForeground( Color.WHITE );
         commentError.setPreferredSize( new Dimension( 170, 25) );
+        commentError.addActionListener(new ActionListener() {
+        	public void actionPerformed(ActionEvent e) {
+                EditorAreaPanel.addHighlight( Color.black);
+                CommentShowPanel.addComment( "Line ... : Commment Error." );
+        	}
+
+        });
         add ( commentError );
         
     }
