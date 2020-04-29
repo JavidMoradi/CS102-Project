c424deb0e9de19c663e879c2f5c7c2bcf5c7186f
haissamf <64235009+haissamf@users.noreply.github.com>
2020-04-29 12:37:08 +0300

    Update CommentShowPanel.java

--- CommentShowPanel.java
+++ CommentShowPanel.java
@@ -1,30 +1,63 @@
+
 import javax.swing.*;
+import javax.swing.event.ListDataEvent;
+import javax.swing.event.ListDataListener;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+
 import java.awt.*;
 
 public class CommentShowPanel extends JPanel
 {
-    static JTextArea commentJTextArea;
-   static String comments = "";
+   public static DefaultListModel model;
+   static JList listFiles;
+   CommentsModel commentsModel;
 
    public CommentShowPanel() {
+	   commentsModel = new CommentsModel();
+	   
+	   model = new DefaultListModel();
+	   listFiles = new JList(model);
+	   listFiles.setPreferredSize( new Dimension( 500, 500 ) );
+       
+       JScrollPane scrollPane = new JScrollPane();
+       scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+       scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+       scrollPane.setViewportView(listFiles);
+ 
+       listFiles.setFont(new Font("Tahoma", Font.PLAIN, 20));
+       add(scrollPane);
+       
+       listFiles.getSelectionModel().addListSelectionListener( new ListSelectionListener() {
+		@Override
+		public void valueChanged(ListSelectionEvent e) {
+			// TODO Auto-generated method stub
+			if( (Comment)listFiles.getSelectedValue() != null)
+				CommentsModel.pointTheComment( (Comment)listFiles.getSelectedValue());
+		} 
+       });
 
-      commentJTextArea = new JTextArea("Line 5: You should avoid doing mistakes\r\n"
-            + "Line 15: Better to use JButton instead of Button\r\n"
-            + "Line 34: Names does not make sense! \r\n");
-      commentJTextArea.setVisible(true);
-      commentJTextArea.setRows(24);
-      commentJTextArea.setColumns( 25 );
-      commentJTextArea.setWrapStyleWord(true);
-      commentJTextArea.setLineWrap( true );
-      commentJTextArea.setFont(new Font("Tahoma", Font.PLAIN, 19));
-      JScrollPane scrollPane = new JScrollPane(commentJTextArea);
-
-      add(scrollPane);
+       add(scrollPane);
+	   add(listFiles);
+	   
+      //commentJTextArea = new JTextArea("Line 5: You should avoid doing mistakes\r\n"
+        //    + "Line 15: Better to use JButton instead of Button\r\n"
+          //  + "Line 34: Names does not make sense! \r\n");
+      //commentJTextArea.setVisible(true);
+      //commentJTextArea.setRows(19);
+      //commentJTextArea.setColumns(40);
+     // commentJTextArea.setWrapStyleWord(true);
+      //commentJTextArea.setFont(new Font("Tahoma", Font.PLAIN, 19));
+      //JScrollPane scrollPane = new JScrollPane(commentJTextArea);
+      //add(scrollPane);
    }
 
-   public static void addComment(String str) {
-      comments += str + "\r\n"  ;
-      commentJTextArea.setText(comments);
-    }
-
+   public static void update() {
+	   
+	   if( model != null) {
+		   model.removeAllElements();
+		   	for(int i = 0; i < CommentsModel.commentsBag.size(); i++) 
+		   		model.addElement(CommentsModel.commentsBag.get(i));
+	   }
+   }
 }
