f1bd949d386a1e5e7483740fae4d5a155eb42f37
tuna-ogut <63511100+tuna-ogut@users.noreply.github.com>
2020-05-01 22:13:28 +0300

    funtioning fileExplorerPanel and some extra features to buttons

--- FileExplorerPanel.java
+++ FileExplorerPanel.java
@@ -1,26 +1,69 @@
 import javax.swing.*;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 import java.awt.*;
+import java.io.File;
 
-public class FileExplorerPanel extends JPanel
-{
+
+public class FileExplorerPanel extends JPanel implements ListSelectionListener {
+
     public static DefaultListModel model;
-    
-    public FileExplorerPanel ()
-    {
-        setPreferredSize( new Dimension( 200, 220 ) );
-        JScrollPane scrollPane = new JScrollPane();
+    JTextField fileName;
+    public static JList lstFiles;
+    int index;
+    File selectedFile;
+
+
+    public FileExplorerPanel() {
+        setPreferredSize(new Dimension(200, 220));
+
+        model = new DefaultListModel();
+        lstFiles = new JList(model);
+        lstFiles.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        lstFiles.setSelectedIndex(0);
+        lstFiles.addListSelectionListener(this);
+        JScrollPane scrollPane = new JScrollPane(lstFiles);
+
         scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
         scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
-        add(scrollPane);
-        
-        model = new DefaultListModel();
-        JList lstFiles = new JList(model);
-        model.addElement("File Name #0.java");
-        model.addElement( "File Name #1.java");
-        lstFiles.setPreferredSize( new Dimension( 180, 150 ) );
-        
+        this.add(scrollPane);
+
+        //model.addElement("File Name #0.java");
+        //model.addElement("File Name #1.java");
+
+        lstFiles.setPreferredSize(new Dimension(180, 150));
         lstFiles.setFont(new Font("Tahoma", Font.PLAIN, 20));
         scrollPane.setViewportView(lstFiles);
         lstFiles.setBackground(new Color(204, 0, 102));
+
+        fileName = new JTextField(10);
+    }
+
+    @Override
+    public void valueChanged(ListSelectionEvent e) 
+    {
+
+        if (e.getValueIsAdjusting() ) 
+        {
+            //FileOptionsPanel.displayArea.setContent( lstFiles.getSelectedValue().toString());
+            int i = model.indexOf( lstFiles.getSelectedValue() );
+            FileOptionsPanel.displayArea.setContent(FileOptionsPanel.getFileContent(i));
+            
+            if(FileOptionsPanel.getFileContent(i) == null)
+            {
+                FileOptionsPanel.displayArea.setContent("");
+            }
+             
+       }
+        add(lstFiles);
+    setVisible(true);
+
     }
 }
+
+
+
+
+  
+    
+
--- FileOptionsPanel.java
+++ FileOptionsPanel.java
@@ -1,10 +1,13 @@
 import javax.swing.*;
+import javax.swing.event.ListSelectionEvent;
+
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class FileOptionsPanel extends JPanel implements ActionListener {
@@ -15,23 +18,23 @@ public class FileOptionsPanel extends JPanel implements ActionListener {
     JFileChooser chooser;
     Scanner scan;
     String extension;
-    String fileName;
+    static String fileName;
     File selectedFile;
-    EditorAreaPanel displayArea;
-    String fileContent;
+    static EditorAreaPanel displayArea;
+    static String fileContent;
     FileWriter fileWriter;
     String filePath;
+    static ArrayList<String> fileContents;
 
-    public FileOptionsPanel()
-    {
+    public FileOptionsPanel() {
 
         newFile = new JButton(" New File \u2795 ");
         saveFile = new JButton(" Save File \uD83D\uDCBE ");
         openFile = new JButton(" Open File ");
         closeFile = new JButton(" Close File ");
 
-        setLayout( new FlowLayout() );
-        setPreferredSize( new Dimension( 655, 500 ) );
+        setLayout(new FlowLayout());
+        setPreferredSize(new Dimension(655, 500));
         setBackground(Color.BLUE);
         setLocation(500, 500);
 
@@ -49,127 +52,151 @@ public class FileOptionsPanel extends JPanel implements ActionListener {
 
         displayArea = new EditorAreaPanel();
         add(displayArea);
-        
 
         fileContent = "";
         selectedFile = null;
         filePath = "";
-
+        fileContents = new ArrayList<>();
 
     }
 
-    public void actionPerformed(ActionEvent actionEvent)
-    {
-        try
-        {
-            //sets the feel to the user's respective machine
-            UIManager.setLookAndFeel( UIManager.getSystemLookAndFeelClassName() );
-            } catch (Exception e) {
+    public void actionPerformed(ActionEvent actionEvent) {
+        try {
+            // sets the feel to the user's respective machine
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception e) {
             JOptionPane.showMessageDialog(null, " Error ");
         }
 
-        if ( actionEvent.getActionCommand().equals( newFile.getText() ) ) // The Action Listener For The "New File" Button
+        if (actionEvent.getActionCommand().equals(newFile.getText())) // The Action Listener For The "New File" Button
         {
-            fileName = JOptionPane.showInputDialog( " Please Enter The Name of The New File " );
-            
-            chooser = new JFileChooser();
-            chooser.setFileSelectionMode( JFileChooser.FILES_AND_DIRECTORIES) ;
+            fileName = JOptionPane.showInputDialog(" Please Enter The Name of The New File ");
 
-            if ( chooser.showOpenDialog( null ) == JFileChooser.APPROVE_OPTION )
-            {
+            chooser = new JFileChooser();
+            chooser.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
+
+            if (chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
                 try {
                     filePath = chooser.getSelectedFile().getAbsolutePath();
-                    System.out.println( filePath );
+                    System.out.println(filePath);
                     selectedFile = new File(filePath);
-                    //selectedFile.getParentFile().mkdirs();
-                    selectedFile.createTempFile( " Test1 ", ".java", selectedFile );
+                    // selectedFile.getParentFile().mkdirs();
+                    selectedFile.createTempFile(" Test1 ", ".java", selectedFile);
+                    FileExplorerPanel.model.addElement(fileName);
 
-                } catch ( IOException e )
-                {
-                    JOptionPane.showMessageDialog(this, " The File Could Not Be Created, Please Try Again ",
-                                "WARNING", JOptionPane.WARNING_MESSAGE);
+                } catch (IOException e) {
+                    JOptionPane.showMessageDialog(this, " The File Could Not Be Created, Please Try Again ", "WARNING",
+                            JOptionPane.WARNING_MESSAGE);
                 }
 
             }
 
-        } else if ( actionEvent.getActionCommand().equals( saveFile.getText() ) ) // The Action Listener For The "Save File" Button
+        } else if (actionEvent.getActionCommand().equals(saveFile.getText())) // The Action Listener For The "Save File"
+                                                                              // Button
         {
-            if ( selectedFile == null )
-            {
-                JOptionPane.showMessageDialog(this, " There is no File To Save, Please Open or Create A New File ",
-                                "WARNING", JOptionPane.WARNING_MESSAGE);
-                  
-            }
-            else
-            {
-                try {
-                    fileWriter = new FileWriter( selectedFile );
-                    fileWriter.write( displayArea.getContent() );
-                    JOptionPane.showMessageDialog(this, " The File Was Saved Successfully ",
-                                "WARNING", JOptionPane.INFORMATION_MESSAGE);
-                    fileWriter.close();
-                } catch ( IOException e )
-                {
-                    JOptionPane.showMessageDialog(this, " The File Was Not Saved, Please Try Again ",
-                                "NOTE", JOptionPane.WARNING_MESSAGE);
-                }
-            }
+            saveFile();
 
-
-        } else if ( actionEvent.getActionCommand().equals( openFile.getText() ) ) // The Action Listener For The "Open File" Button
+        } else if (actionEvent.getActionCommand().equals(openFile.getText())) // The Action Listener For The "Open File"
+                                                                              // Button
         {
 
             try {
                 chooser = new JFileChooser();
                 chooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
 
-                if ( chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION )
-                {
+                if (chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
                     fileContent = "";
                     selectedFile = chooser.getSelectedFile();
 
-                    scan = new Scanner ( selectedFile );
+                    scan = new Scanner(selectedFile);
                     fileName = selectedFile.getName();
-                    extension = fileName.substring( fileName.lastIndexOf(".") ); // gets the extension
+                    extension = fileName.substring(fileName.lastIndexOf(".")); // gets the extension
 
-                    if (!( extension.equals(".java") ) && !( extension.equals(".txt") ) ) //checks the type of the selected file
+                    if (!(extension.equals(".java")) && !(extension.equals(".txt"))) // checks the type of the selected
+                                                                                     // file
                     {
-                        JOptionPane.showMessageDialog(this, " The File You Have Chosen is Invalid," + "\n"
-                                        + " Please Select A Java File (.java) "
-                                        + "or A Text Document (.txt) ",
+                        JOptionPane.showMessageDialog(
+                                this, " The File You Have Chosen is Invalid," + "\n"
+                                        + " Please Select A Java File (.java) " + "or A Text Document (.txt) ",
                                 "WARNING", JOptionPane.WARNING_MESSAGE);
                     } else {
-                        FileExplorerPanel.model.addElement( fileName );
+                        FileExplorerPanel.model.addElement(fileName);
 
-                       
-                        while ( scan.hasNextLine() )
-                        {
+                        while (scan.hasNextLine()) {
                             fileContent += scan.nextLine() + "\n";
                         }
+                    //fileContents.add(fileContent);
                     }
-                    displayArea.setContent( fileContent ); // Displays the contents of the file
+                    displayArea.setContent(fileContent); // Displays the contents of the file
+                    fileContents.add(fileContent);
 
                 }
-            } catch ( IOException ex ) {
-                JOptionPane.showMessageDialog( null, " Error " );
+            } catch (IOException ex) {
+                JOptionPane.showMessageDialog(null, " Error ");
             }
 
-        } else if  ( actionEvent.getActionCommand().equals( closeFile.getText() ) ) // The Action Listener For The "Close File" Button
+        } else if (actionEvent.getActionCommand().equals(closeFile.getText())) // The Action Listener For The "Close
+                                                                               // File" Button
         {
-            if ( FileExplorerPanel.model.size() > 0 ) {
-                FileExplorerPanel.model.remove(FileExplorerPanel.model.size() - 1);
-                displayArea.setContent( "" );
+            if (FileExplorerPanel.model.size() > 0) {
+                int index = FileExplorerPanel.model.indexOf(FileExplorerPanel.lstFiles.getSelectedValue());
+                FileExplorerPanel.model.remove(index);//FileExplorerPanel.model.indexOf(FileExplorerPanel.lstFiles.getSelectedValue()));                       
+                fileContents.remove(index);//FileExplorerPanel.model.indexOf(FileExplorerPanel.lstFiles.getSelectedValue()));
+
+                if(fileContents.size() == 0)
+                {
+                   displayArea.setContent("");
+                }
+                else
+                {
+                   if( index == 0 && fileContents.size() > 0 )
+                   {
+                        displayArea.setContent(getFileContent( 0 ));
+                   }
+                   else
+                   {
+                      displayArea.setContent(getFileContent( index-1 ));
+                   }
             }
-            else
-            {
-                JOptionPane.showMessageDialog( this, " There Are No Files To Close ",
-                    "WARNING", JOptionPane.WARNING_MESSAGE );
+            } else {
+                JOptionPane.showMessageDialog(this, " There Are No Files To Close ", "WARNING",
+                        JOptionPane.WARNING_MESSAGE);
             }
 
         } else {
+
+            JOptionPane.showMessageDialog(this, " The Button You Have Pressed is Invalid ", "WARNING",
+                    JOptionPane.WARNING_MESSAGE);
+        }
+    }
+
+    public static String getFileContent(int index)
+     {
+        return fileContents.get(index);
+    }
+    
+    public void saveFile ()
+    {
+        if (selectedFile == null) {
+            JOptionPane.showMessageDialog(this, " There is no File To Save, Please Open or Create A New File ",
+                    "WARNING", JOptionPane.WARNING_MESSAGE);
+        
+        } else {
+            try {
             
-            JOptionPane.showMessageDialog( this, " The Button You Have Pressed is Invalid ",
-                    "WARNING", JOptionPane.WARNING_MESSAGE );
+                fileWriter = new FileWriter(selectedFile);
+                fileWriter.write(displayArea.getContent());
+                JOptionPane.showMessageDialog(this, " The File Was Saved Successfully ",
+                        "NOTE", JOptionPane.INFORMATION_MESSAGE);
+                fileWriter.close();
+            } catch (IOException e) {
+                JOptionPane.showMessageDialog(this, " The File Was Not Saved, Please Try Again ",
+                        "WARNING", JOptionPane.WARNING_MESSAGE);
+            }
         }
     }
 }
+
+
+
+
