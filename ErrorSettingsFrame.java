652443ed9f4da936d71051f6af64781f0ce15e6c
AtasagunSanap <63560730+AtasagunSanap@users.noreply.github.com>
2020-05-08 23:39:48 +0300

    Add files via upload

---
+++ ErrorSettingsFrame.java
@@ -0,0 +1,22 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class ErrorSettingsFrame extends JFrame {
+
+    ErrorSettingPanel settings;
+    public ErrorSettingsFrame( NewCommentPanel commentPanel) {
+        super();
+        setPreferredSize( new Dimension( 1080, 720 ) );;
+        settings = new ErrorSettingPanel( commentPanel);
+        add( settings);
+
+        setLayout ( new FlowLayout() );
+
+        pack();
+        setVisible(true);
+    }
+
+
+
+
+}
