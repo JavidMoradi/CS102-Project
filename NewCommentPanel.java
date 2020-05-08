1afadcd6ea6de34402d1bbb161d1b9e8f0ea93ec
AtasagunSanap <63560730+AtasagunSanap@users.noreply.github.com>
2020-05-08 23:47:56 +0300

    The colors of buttons and highlights
    The colors of buttons and highlights are organized and added into an ArrayList to later on access and change in errorOptionsPanel

--- NewCommentPanel.java
+++ NewCommentPanel.java
@@ -3,10 +3,14 @@ import javax.swing.border.LineBorder;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 public class NewCommentPanel extends JPanel
 {
-    EditorAreaPanel display = new EditorAreaPanel();
+    EditorAreaPanel display;
+    ErrorSettingsFrame errorSettings;
+    NewCommentPanel thisPanel;
+    static ArrayList<Color> errorColors;
 
     public NewCommentPanel ( EditorAreaPanel display )
     {
@@ -25,144 +29,148 @@ public class NewCommentPanel extends JPanel
         insertLabel.setHorizontalAlignment( SwingConstants.CENTER );
         insertLabel.setBounds( 200, 36, 201, 40 );
         add ( insertLabel );
+        
+        errorColors = new ArrayList<Color>();
 
-        JButton wrongIndentationError = new JButton(" Wrong Indentation");
-        wrongIndentationError.setBackground(Color.RED);
+        JButton errorSettingsButton = new JButton();
+        errorSettingsButton.setBackground(Color.GRAY);
+        errorSettingsButton.setForeground( Color.WHITE );
+        errorSettingsButton.setPreferredSize( new Dimension( 15, 15) );
+        add ( errorSettingsButton );
+        thisPanel = this;
+        errorSettingsButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                errorSettings = new ErrorSettingsFrame( thisPanel);
+            }
+        });
+        
+        JButton wrongIndentationError = new JButton("Wrong Indentation");
+        Color wrongIndentationColor = Color.RED;
+        wrongIndentationError.setBackground(wrongIndentationColor);
         wrongIndentationError.setForeground( Color.WHITE );
         wrongIndentationError.setPreferredSize( new Dimension( 190, 25) );
         wrongIndentationError.addActionListener(new ActionListener() {
-        	public void actionPerformed(ActionEvent e) {
-        		Color color;
-        		Comment c;
-
-        		color = Color.RED;
-        		c = new Comment( "Wrong Indentation", display.getSelectionFirst(), display.getSelectionLast(), display.getLineNumber(display.getSelectionFirst()), color);
-
-        		display.addHighlight( color );
+            public void actionPerformed(ActionEvent e) {
+                Comment c;
+                c = new Comment( "Wrong Indentation", display.getSelectionFirst(), display.getSelectionLast(), display.getLineNumber(display.getSelectionFirst()), wrongIndentationColor);
+                display.addHighlight( wrongIndentationColor );
                 //add comment with 5 parameters!
-        		CommentsModel.addComment(c);
+                CommentsModel.addComment(c);
                 CommentShowPanel.update();
-        	}
-
-
+            }
         });
         add ( wrongIndentationError );
+        errorColors.add( wrongIndentationColor);
 
         JButton inefficientError = new JButton("Inefficient Code");
-        inefficientError.setBackground(Color.BLUE);
+        Color inefficientErrorColor = Color.BLUE;
+        inefficientError.setBackground(inefficientErrorColor);
         inefficientError.setForeground( Color.WHITE );
         inefficientError.setPreferredSize( new Dimension( 170, 25) );
         inefficientError.addActionListener(new ActionListener() {
         	public void actionPerformed(ActionEvent e) {
 
-                display.addHighlight( Color.BLUE);
-        		Color color;
+                display.addHighlight( inefficientErrorColor);
         		Comment c;
 
-        		color = Color.BLUE;
-        		display.addHighlight( color );
+        		display.addHighlight( inefficientErrorColor );
 
-        		c = new Comment( "Inefficient Code Error", display.getSelectionFirst(), display.getSelectionLast(), display.getLineNumber(display.getSelectionFirst()), color);
+        		c = new Comment( "Inefficient Code Error", display.getSelectionFirst(), display.getSelectionLast(), display.getLineNumber(display.getSelectionFirst()), inefficientErrorColor);
 
                 //add comment with 5 parameters!
         		CommentsModel.addComment(c);
                 CommentShowPanel.update();
                // CommentShowPanel.addComment( "Line: " + display.getLineNumber(display.getSelectionFirst()) + " Inefficient Code Error." );
         	}
-
         });
         add ( inefficientError );
+        errorColors.add( inefficientErrorColor);
 
         JButton namingConventionError = new JButton("Naming Conventions Error");
-        namingConventionError.setBackground(Color.MAGENTA);
+        Color namingConventionColor = Color.MAGENTA;
+        namingConventionError.setBackground( namingConventionColor);
         namingConventionError.setForeground( Color.WHITE );
         namingConventionError.setPreferredSize( new Dimension( 190, 25) );
         namingConventionError.addActionListener(new ActionListener() {
         	public void actionPerformed(ActionEvent e) {
 
-        		Color color;
         		Comment c;
 
-        		color = Color.MAGENTA;
-        		display.addHighlight( color);
-        		c = new Comment( "Naming Convention Error", display.getSelectionFirst(), display.getSelectionLast(), display.getLineNumber(display.getSelectionFirst()), color);
+        		display.addHighlight( namingConventionColor);
+        		c = new Comment( "Naming Convention Error", display.getSelectionFirst(), display.getSelectionLast(), display.getLineNumber(display.getSelectionFirst()), namingConventionColor);
 
                 //add comment with 5 parameters!
         		CommentsModel.addComment(c);
                 CommentShowPanel.update();
               //  CommentShowPanel.addComment( "Line: " + display.getLineNumber(display.getSelectionFirst()) + " Nameing Convention Error." );
         	}
-
         });
         add ( namingConventionError );
+        errorColors.add(namingConventionColor);
 
         JButton javaDocError = new JButton("JavaDoc Error");
-        javaDocError.setBackground(new Color(24, 147, 196));
+        Color javaDocColor = new Color(24, 147, 196);
+        javaDocError.setBackground(javaDocColor);
         javaDocError.setForeground( Color.WHITE );
         javaDocError.setPreferredSize( new Dimension( 170, 25) );
         javaDocError.addActionListener(new ActionListener() {
         	public void actionPerformed(ActionEvent e) {
-                Color color;
         		Comment c;
-
-        		color = new Color(24, 147, 196);
-        		display.addHighlight( color);
-        		c = new Comment( "JavaDoc Comment Error" , display.getSelectionFirst(), display.getSelectionLast(), display.getLineNumber(display.getSelectionFirst()), color);
+        		display.addHighlight( javaDocColor);
+        		c = new Comment( "JavaDoc Comment Error" , display.getSelectionFirst(), display.getSelectionLast(), display.getLineNumber(display.getSelectionFirst()), javaDocColor);
 
                 //add comment with 5 parameters!
         		CommentsModel.addComment(c);
                 CommentShowPanel.update();
               //  CommentShowPanel.addComment( "Line: " + display.getLineNumber(display.getSelectionFirst()) + " JavaDoc Comment Error." );
         	}
-
         });
         add ( javaDocError );
+        errorColors.add(javaDocColor);
 
         JButton styleErrorButton = new JButton("Blank Line/Space Error");
-        styleErrorButton.setBackground(Color.gray);
+        Color styleColor = Color.GRAY;
+        styleErrorButton.setBackground( styleColor);
         styleErrorButton.setForeground( Color.WHITE );
         styleErrorButton.setPreferredSize( new Dimension( 190, 25) );
         styleErrorButton.addActionListener(new ActionListener() {
         	public void actionPerformed(ActionEvent e) {
-                Color color;
-        		Comment c;
+                Comment c;
+        		c = new Comment( "Style Error", display.getSelectionFirst(), display.getSelectionLast(), display.getLineNumber(display.getSelectionFirst()), styleColor);
 
-        		color = Color.gray;
-        		display.addHighlight( color);
-        		c = new Comment( "Style Error", display.getSelectionFirst(), display.getSelectionLast(), display.getLineNumber(display.getSelectionFirst()), color);
-
-        		display.addHighlight( color );
+        		display.addHighlight( styleColor );
                 //add comment with 5 parameters!
         		CommentsModel.addComment(c);
                 CommentShowPanel.update();
              //   CommentShowPanel.addComment( "Line: " + display.getLineNumber(display.getSelectionFirst()) + " Style Error." );
         	}
-
         });
         add ( styleErrorButton );
+        errorColors.add(styleColor);
 
         JButton commentError = new JButton("Comment Error");
-        commentError.setBackground(new Color( 29, 171, 34 ));
+        Color commentErrorColor = new Color( 29, 171, 34 );
+        commentError.setBackground(commentErrorColor);
         commentError.setForeground( Color.WHITE );
         commentError.setPreferredSize( new Dimension( 170, 25) );
         commentError.addActionListener(new ActionListener() {
         	public void actionPerformed(ActionEvent e) {
-                Color color;
         		Comment c;
 
-        		color = new Color( 29, 171, 34 );
-        		display.addHighlight( color);
+        		c = new Comment( "Commment Error", display.getSelectionFirst(), display.getSelectionLast(), display.getLineNumber(display.getSelectionFirst()), commentErrorColor);
 
-        		c = new Comment( "Commment Error", display.getSelectionFirst(), display.getSelectionLast(), display.getLineNumber(display.getSelectionFirst()), color);
-
-        		display.addHighlight( color );
+        		display.addHighlight( commentErrorColor );
                 //add comment with 5 parameters!
         		CommentsModel.addComment(c);
                 CommentShowPanel.update();
            //     CommentShowPanel.addComment( "Line: " + display.getLineNumber(display.getSelectionFirst()) + " Commment Error." );
         	}
-
         });
         add ( commentError );
+        errorColors.add(commentErrorColor);
+    }
+    
+    public static ArrayList<Color> getColors() {
+        return errorColors;
     }
 }
