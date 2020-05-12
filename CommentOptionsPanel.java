54ff09b5a5cf2d2bc27da5e3b3377b762e633c27
oavci2000 <63664598+oavci2000@users.noreply.github.com>
2020-05-12 23:26:32 +0300

    Update CommentOptionsPanel.java

--- CommentOptionsPanel.java
+++ CommentOptionsPanel.java
@@ -7,6 +7,7 @@ import java.awt.event.ActionListener;
 public class CommentOptionsPanel extends JPanel
 {
 	EditorAreaPanel display;
+	InsertACommentFrame insertACommentFrame;
 
 	 public CommentOptionsPanel ( EditorAreaPanel display )
 	    {
@@ -36,21 +37,7 @@ public class CommentOptionsPanel extends JPanel
 
 				@Override
 				public void actionPerformed(ActionEvent e) {
-	                JPanel fields = new JPanel(new GridLayout(2, 1));
-	                JTextField field = new JTextField(10);
-	                JComboBox<String> comboBox = new JComboBox<>(new String[]{"Color 1", "Color 2", "Color 3"});
-
-	                fields.add(field);
-	                fields.add(comboBox);
-
-	                int result = JOptionPane.showConfirmDialog(null, fields, "Insert Comment", JOptionPane.OK_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE);
-	                switch (result) {
-	                    case JOptionPane.OK_OPTION:
-	                        // Process the results...
-	                        System.out.println(field.getText());
-	                        System.out.println( comboBox.getSelectedItem());
-	                        break;
-	                }
+	                	insertACommentFrame = new InsertACommentFrame(display);
 				}
 
 	        });
