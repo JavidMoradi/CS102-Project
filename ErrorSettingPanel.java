57c2dfd6278fbc9d0d7978430793f7d48d1c5d02
AtasagunSanap <63560730+AtasagunSanap@users.noreply.github.com>
2020-05-19 21:27:25 +0300

    Update ErrorSettingPanel.java

--- ErrorSettingPanel.java
+++ ErrorSettingPanel.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import javax.swing.border.Border;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 import javax.swing.event.ListSelectionEvent;
@@ -8,12 +9,11 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
 
-public class ErrorSettingPanel extends JPanel
-{
+public class ErrorSettingPanel extends JPanel {
 
     static Color selectedColor;
     static int selectedErrorPos;
-    String[] ERRORS = { "Wrong Indentation", "Inefficient Code", "Naming Conventions Error", "JavaDoc Error", "Blank Line/Space Error", "Comment Error" };
+    String[] errors = {"Wrong Indentation", "Inefficient Code", "Naming Conventions Error", "JavaDoc Error", "Blank Line/Space Error", "Comment Error"};
     JColorChooser colorChooser;
     String selectedError;
     DefaultListModel model;
@@ -21,64 +21,80 @@ public class ErrorSettingPanel extends JPanel
     JScrollPane scrollPane;
     JTextField renameField;
     JLabel rename;
-    JButton applyButton;
+    JButton applyButton, okButton, cancelButton;
 
     ColorAndText colorAndText;
     NewCommentPanel commentPanel;
 
-    public ErrorSettingPanel ( NewCommentPanel commentPanel )
-    {
+    JFrame frame;
+
+
+    public ErrorSettingPanel(NewCommentPanel commentPanel, ErrorSettingsFrame frame) {
         this.commentPanel = commentPanel;
+        this.frame = frame;
 
-        setLayout ( new BorderLayout () );
-        model = new DefaultListModel ();
-        errorTypes = new JList ( model );
-        errorTypes.setSelectionMode ( ListSelectionModel.SINGLE_SELECTION );
+        setLayout(new BorderLayout());
+        model = new DefaultListModel();
+        errorTypes = new JList(model);
+        errorTypes.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 
-        for ( int error = 0; error < ERRORS.length; error++ )
-        {
-            model.addElement ( ERRORS[error] );
-        }
+        for (int error = 0; error < errors.length; error++)
+            model.addElement(errors[error]);
 
-        errorTypes.setSelectedIndex ( 0 );
-        errorTypes.addListSelectionListener ( new ErrorSelectionListener () );
+        errorTypes.setSelectedIndex(0);
+        errorTypes.addListSelectionListener(new ErrorSelectionListener());
 
-        scrollPane = new JScrollPane ( errorTypes );
-        scrollPane.setPreferredSize ( new Dimension ( 200, 200 ) );
-        add ( scrollPane, BorderLayout.WEST );
-
-
-        errorTypes.setPreferredSize ( new Dimension ( 180, 150 ) );
-        errorTypes.setFont ( new Font ( "Tahoma", Font.PLAIN, 20 ) );
-        scrollPane.setViewportView ( errorTypes );
-        errorTypes.setBackground ( new Color ( 204, 0, 102 ) );
+        scrollPane = new JScrollPane(errorTypes);
+        scrollPane.setPreferredSize(new Dimension(200, 200));
+        add(scrollPane, BorderLayout.WEST);
 
 
-        colorAndText = new ColorAndText ();
-        colorAndText.setPreferredSize ( new Dimension ( 600, 360 ) );
-        add ( colorAndText, BorderLayout.EAST );
+        errorTypes.setPreferredSize(new Dimension(180, 150));
+        errorTypes.setFont(new Font("Tahoma", Font.PLAIN, 20));
+        scrollPane.setViewportView(errorTypes);
+        errorTypes.setBackground(new Color(204, 0, 102));
 
-        applyButton = new JButton ( "Apply" );
-        applyButton.setPreferredSize ( new Dimension ( 100, 40 ) );
-        add ( applyButton, BorderLayout.SOUTH );
-        applyButton.addActionListener ( new ApplyListener () );
+
+        colorAndText = new ColorAndText();
+        colorAndText.setPreferredSize(new Dimension(600, 360));
+        add(colorAndText, BorderLayout.EAST);
+
+        JPanel buttonPanel = new JPanel();
+        add( buttonPanel, BorderLayout.SOUTH);
+        buttonPanel.setLayout( new BorderLayout());
+        cancelButton = new JButton("Cancel");
+        cancelButton.setPreferredSize(new Dimension(100, 40));
+        buttonPanel.add( cancelButton, BorderLayout.WEST );
+        cancelButton.addActionListener( new CancelListener());
+
+        JPanel applyAndOkButtonPanel = new JPanel();
+        buttonPanel.add( applyAndOkButtonPanel, BorderLayout.EAST);
+        applyAndOkButtonPanel.setLayout(new FlowLayout());
+        applyButton = new JButton("Apply");
+        applyButton.setPreferredSize(new Dimension(100, 40));
+        applyAndOkButtonPanel.add(applyButton);
+        applyButton.addActionListener(new ApplyListener());
+
+        okButton = new JButton("Ok!");
+        okButton.setPreferredSize(new Dimension(100, 40));
+        applyAndOkButtonPanel.add(okButton);
+        okButton.addActionListener(new OkListener());
+
 
     }
 
-    private class ColorAndText extends JPanel
-    {
-        public ColorAndText ()
-        {
-            colorChooser = new JColorChooser ();
-            add ( colorChooser, BorderLayout.NORTH );
-            colorChooser.getSelectionModel ().addChangeListener ( new ColorChangeListener () );
+    private class ColorAndText extends JPanel {
+        public ColorAndText() {
+            colorChooser = new JColorChooser();
+            add(colorChooser, BorderLayout.NORTH);
+            colorChooser.getSelectionModel().addChangeListener(new ColorChangeListener());
 
-            rename = new JLabel ( "Rename your error: " );
-            add ( rename, BorderLayout.WEST );
+            rename = new JLabel("Rename your error: ");
+            add(rename, BorderLayout.WEST);
 
-            renameField = new JTextField ();
-            renameField.setPreferredSize ( new Dimension ( 300, 30 ) );
-            add ( renameField, BorderLayout.SOUTH );
+            renameField = new JTextField();
+            renameField.setPreferredSize(new Dimension(300, 30));
+            add(renameField, BorderLayout.SOUTH);
         }
     }
 
@@ -87,14 +103,11 @@ public class ErrorSettingPanel extends JPanel
         @Override
         public void valueChanged ( ListSelectionEvent e )
         {
-            selectedError = ( String ) errorTypes.getSelectedValue ();
-            for ( int error = 0; error < ERRORS.length; error++ )
-            {
-                if ( ERRORS[error].equals ( selectedError ) )
-                {
+            selectedError = (String) errorTypes.getSelectedValue();
+            for ( int error = 0; error < errors.length; error++ )
+                if ( errors[error].equals ( selectedError ) )
                     selectedErrorPos = error;
-                }
-            }
+            renameField.setText( errors[selectedErrorPos]);
         }
     }
 
@@ -109,16 +122,34 @@ public class ErrorSettingPanel extends JPanel
     private class ApplyListener implements ActionListener
     {
         @Override
-        public void actionPerformed ( ActionEvent e )
-        {
-            try
-            {
-                commentPanel.editColor ( selectedErrorPos, selectedColor );
-                commentPanel.editErrorName ( selectedErrorPos, renameField.getText () );
-            } catch ( IOException ioException )
-            {
-                ioException.printStackTrace ();
+        public void actionPerformed(ActionEvent e) {
+            try {
+                commentPanel.editColor(selectedErrorPos, selectedColor);
+                commentPanel.editErrorName(selectedErrorPos, renameField.getText());
+            } catch (IOException ioException) {
+                ioException.printStackTrace();
             }
+        }
+    }
+
+    private class OkListener implements ActionListener
+    {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            try {
+                commentPanel.editColor(selectedErrorPos, selectedColor);
+                commentPanel.editErrorName(selectedErrorPos, renameField.getText());
+            } catch (IOException ioException) {
+                ioException.printStackTrace();
+            }
+            frame.dispose();
+        }
+    }
+
+    private class CancelListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            frame.dispose();
         }
     }
 }
